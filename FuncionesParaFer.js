58d953df7c0ce96ad03efe5407f5289b11306783
RangoTheStressedGamer2077 <jesusrafaeljt23@gmail.com>
2024-11-05 09:45:18 -0600

    mi tarea

---
+++ FuncionesParaFer.js
@@ -0,0 +1,11 @@
+document.getElementById("registroId").addEventListener("click", function () { //hola mundo
+    // const usuario = document.querySelector("#usuario").value.trim();
+    // const email = document.querySelector("#email").value.trim();
+    // const password = document.querySelector("#password").value.trim();
+    // console.log(usuario);
+    // console.log(email);
+    // console.log(password);
+    console.log("hola mundo")
+})
+alert("hola");
+// console.log("hola mundo");
